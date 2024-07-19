e9b20c79f18556109c2e81a54bf98dc72cac5172
Mr. WinTux <rusok.music@gmail.com>
2024-07-18 21:30:24 -0400

    Primer commit

---
+++ src/main/java/com/wintux/stretch/StretchApplication.java
@@ -0,0 +1,13 @@
+package com.wintux.stretch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StretchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StretchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wintux/stretch/StretchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wintux.stretch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StretchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
